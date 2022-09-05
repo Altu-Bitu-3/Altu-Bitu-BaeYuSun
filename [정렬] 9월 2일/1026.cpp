31e359f6026733fba76ce5b84176030be9bb0bca
Esther012 <106883141+Esther012@users.noreply.github.com>
2022-09-05 12:23:57 +0900

    [정렬] 9월 2일

---
+++ [정렬] 9월 2일/1026.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+bool cmp(int a, int b) {
+	return a > b;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	int a[50] = { 0, };
+	int b[50] = { 0, };
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> b[i];
+	}
+
+	sort(a, a + n);
+	sort(b, b + n, cmp);
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += a[i] * b[i];
+	}
+	cout << sum;
+
+}
