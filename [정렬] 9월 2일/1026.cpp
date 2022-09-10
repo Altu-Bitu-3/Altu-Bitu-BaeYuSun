b9cb3ede7c1d46c66de734a73da0a1efc70c1dc4
Esther012 <106883141+Esther012@users.noreply.github.com>
2022-09-10 20:04:52 +0900

    Update 1026.cpp

--- [정렬] 9월 2일/1026.cpp
+++ [정렬] 9월 2일/1026.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-bool cmp(int a, int b) {
-	return a > b;
-}
 
 int main() {
 	int n;
 	cin >> n;
-	int a[50] = { 0, };
-	int b[50] = { 0, };
-
+	//입력받을 배열 벡터로 선언
+	vector <int> a;
+	vector <int> b;
+	
+	//a배열 입력
 	for (int i = 0; i < n; i++) {
-		cin >> a[i];
+		cin >> num;
+		a.push_back(num);
 	}
+	//b매열 입력
 	for (int i = 0; i < n; i++) {
-		cin >> b[i];
+		cin >> num;
+		b.push_back(num);
 	}
-
-	sort(a, a + n);
-	sort(b, b + n, cmp);
+	//a배열은 오름차순 정렬, b배열은 내림차순 정렬
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end(), greater<>());
 	int sum = 0;
+	//배열을 정렬 한 후 곱해서 최솟값 구하기
 	for (int i = 0; i < n; i++) {
 		sum += a[i] * b[i];
 	}
+	//출력
 	cout << sum;
-
+	return 0;
 }
