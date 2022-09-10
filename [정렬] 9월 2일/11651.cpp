de5002735c6ab2f185c37e8705ad6ef679dfac02
Esther012 <106883141+Esther012@users.noreply.github.com>
2022-09-11 00:51:36 +0900

    Update 11651.cpp

--- [정렬] 9월 2일/11651.cpp
+++ [정렬] 9월 2일/11651.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
-struct cordinate{
-	int x, y;
-};
+using namespace std;
+typedef pair<int, int> cor;
+
 //비교함수
-bool cmp(const cordinate& a, const cordinate& b) {
-	if (a.y != b.y) return a.y < b.y; //y좌표가 다르면  좌표가 증가하는 순서대로 정렬
-	if (a.x != b.x) return a.x < b.x; //x좌표가 다르면 좌표가 증가하는 순서대로 정렬
+bool compare(const cor& a, const cor& b) {
+	if (a.second != b.second) {
+		return a.second < b.second;
+	}
+	else return a.first < b.first;
 }
 
 int main() {
 	int n; //입력받을 좌표의 개수
 
 	cin >> n;
-	vector<cordinate> arr(n);
+	vector<cor> arr(n);
 
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i].x >> arr[i].y;
+		cin >> arr[i].first >> arr[i].second;
 	}
 
-	sort(arr.begin(), arr.end(), cmp);
+	sort(arr.begin(), arr.end(), compare);
 	//출력
 	for (int i = 0; i < n; i++) {
-		cout << arr[i].x <<" " << arr[i].y << "\n";
+		cout << arr[i].first <<" " << arr[i].second << "\n";
 	}
 }
