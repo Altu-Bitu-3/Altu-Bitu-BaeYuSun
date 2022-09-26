e689fe993a394e9eb70b020d791c3f0a5b2259f1
Esther012 <106883141+Esther012@users.noreply.github.com>
2022-09-26 12:35:29 +0900

    Update 1758.cpp

--- [정렬] 9월 2일/1758.cpp
+++ [정렬] 9월 2일/1758.cpp
@@ -2,29 +2,27 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-vector <int> line;
-//내림차순 정렬을 위한 cmp
-bool cmp(int a, int b) {
-	return a > b;
-}
+
 //받을 수 있는 팁의 합계를 계산
-long sum(int n) {
+long sum(int n, vector <int>& line) {
 	long sum = 0;
 	for (int i = 0; i < n; i++) {
-		if (line[i] - i > 0) { //음수가 되면 팁을 받을 수 없기 때문에
-			sum += line[i] - i;
+		if (line[i] - i < 0) { //음수가 되면 팁을 받을 수 없기 때문에
+			break;
 		}
+		sum += line[i] - i;
 	}
 	return sum;
 }
 int main() {
 	int n; //첫째 줄에 서 있는 사람의 수
 	cin >> n;
+	vector <int> line;
 	line.assign(n, 0);
 	for (int i = 0; i < n; i++) {
 		cin >> line[i];
 	}
-	sort(line.begin(), line.end(), cmp);
-	cout << sum(n);
+	sort(line.begin(), line.end(), greater<>());
+	cout << sum(n, line);
 	return 0;
 }
