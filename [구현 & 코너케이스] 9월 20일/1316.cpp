c6d569c6c0ddffc41de7fedab06d6d9ec881ae32
Esther012 <106883141+Esther012@users.noreply.github.com>
2022-09-26 17:53:30 +0900

    Update 1316.cpp

--- [구현 & 코너케이스] 9월 20일/1316.cpp
+++ [구현 & 코너케이스] 9월 20일/1316.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 //그룹단어인지 체크하고 맞으면 true, 아니면 false 반환하는 함수
 bool checkGroup(string word) {
+    char cur = NULL; //현재의 문자
+    vector<bool> alphabet(26, false); // 단어에서 특정 문자가 나온 적 있는지 확인하는 bool형 배열
+    
     //입력받은 문자열의 길이만큼 비교 반복
     for (int i = 0; i < word.size(); i++) {
-        for (int j = 0; j < i; j++) {
-            //연속해서 나타난 문자가 아닐 경우 && 이전에 등장했던 문자지만 떨어져서 나타난 경우
-            if (word[i] != word[i - 1] && word[i] == word[j]) {
+        if (word[i] == cur) { //연속해서 나타난 문자일 경우
+            continue;
+        }
+        //이전의 문자와 다른 문자가 나타났다면
+        if (alphabet[word[i] - 'a']) {  //word[i] - 'a'를 통해 이전의 알파벳과 다른 알파벳인지 비교
                 //그룹 단어가 아니므로 false 반환
                 return false;
             }
-        }
+        cur = word[i];
+        alphabet[word[i] - 'a'] = true;
+        
     }
     //위의 경우에 걸러지지 않았으면 true 반환
     return true;
